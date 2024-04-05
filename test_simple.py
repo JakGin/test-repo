0b65f3943e7589ccaa7f144d7fdd410b2e58a433
Jakub Glinka <jakubglinka9@gmail.com>
2024-04-05 23:11:03 +0200

    Commit

--- test_simple.py
+++ test_simple.py
@@ -1,4 +1,22 @@
-from simple import add
+from simple import add, subtract, multiply, divide
 
-def test_answer():
-  assert add(4, 5) == 9
+
+def test_add():
+    assert add(4, 5) == 9
+
+
+def test_subtract():
+    assert subtract(10, 2) == 8
+
+
+def test_multiply():
+    assert multiply(4, 5) == 20
+
+
+def test_divide():
+    assert divide(10, 2) == 5
+    try:
+        divide(10, 0)
+        assert False, "Expected ZeroDivisionError"
+    except ZeroDivisionError:
+        assert True
